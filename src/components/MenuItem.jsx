f8eedc26c98bce16a9fe38b222d0690306c70431
Babina081 <babitamoo333@gmail.com>
2024-05-11 16:05:56 +0545

    ADD menuitems

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,9 +4,8 @@ import React from "react";
 export default function MenuItem({ title, address, Icon }) {
   return (
     <Link href={address} className="hover:text-amber-500">
-      <Icon className='text-2xl sm:hidden'></Icon>
+      <Icon className="text-2xl sm:hidden"></Icon>
       <p className="uppercase hidden sm:inline text-sm">{title}</p>
-
     </Link>
   );
 }
