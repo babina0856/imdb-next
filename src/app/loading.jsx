cdb998b46b4c9b181b00752b5c765f2983490dc0
Babina081 <babitamoo333@gmail.com>
2024-05-23 22:38:58 +0545

    Add card component

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className="flex justify-center mt-16">
-      <Image className="h-52" src="loading.svg" alt="loading..." />
+      <Image className="h-52" src="/loading.svg" width={43} height={43} alt="loading..." />
     </div>
   );
 }
